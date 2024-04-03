9d20b48edd55a4e5d8f7fdd9d5f71637e7ba2b4d
HorroGlitch <lverot@edenschool.fr>
2024-04-03 11:43:25 +0200

    finished alogorithm

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const app = document.querySelector(".container");
 
-setupCounter(document.querySelector('#counter'))
+function damier(num) {
+  for (let i = 0; i < num; i++) {
+    const ligne = document.createElement("div");
+    ligne.classList.add("row")
+    app.appendChild(ligne)
+
+    for (let j = 0; j < num; j++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell")
+
+    if ((i+j)%2 === 0) {
+      cell.classList.add("black")
+    }
+    
+    ligne.appendChild(cell)
+      
+    }
+  }
+}
+damier(9)
